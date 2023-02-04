4fcb9544d250a1b29a023e8b4160a4d88ba2b5c1
colespen <spencerlewiscole@gmail.com>
2023-02-03 19:42:33 -0500

    express static path

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /////   serve static files for build ******
-// app.use(express.static(path.resolve(__dirname, '../../client/build')));
+app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
 let port = process.env.PORT || 8001;
 
