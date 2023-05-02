0083bc005bb257251e21410252c4d5a6a8e30a9f
Marcus Ragaller <marcus@echoblue.io>
2023-05-02 09:47:59 +0200

    fixes incorrect naming convention for route structure

--- server/src/routes/shows.route.ts
+++ server/src/routes/shows.route.ts
@@ -2,19 +2,30 @@ import express from 'express';
 import Geohash from 'latlon-geohash';
 import Debug from 'debug';
 import TicketMasterService from '../services/ticketmaster.service.js';
+import createError from 'http-errors';
 
 const debug = Debug('sf-api:route:shows');
 const router = express.Router();
+const rootRoute = '/shows';
 
 /** query shows based on search terms
  */
-router.get('/shows', async (req, res) => {
-  const geoPoint = Geohash.encode(req.body.lat, req.body.lng, 9);
-  const events = await TicketMasterService.eventSearch(geoPoint);
-  res.send({
-    geoPoint,
-    events: events.data,
-  });
+router.get(`${rootRoute}`, async (req, res, next) => {
+  try {
+    const geoPoint = Geohash.encode(Number(req.query.lat), Number(req.query.lng), 9);
+    const events = await TicketMasterService.eventSearch(geoPoint);
+    res.send({
+      geoPoint,
+      events: events.data,
+    });
+  } catch(e: any) {
+    const msg = e?.message?.toLowerCase();
+    if (msg == 'invalid geohash') {
+      next(createError(417, 'query paramaters \'lat\' or \'lng\' missing'));
+      return;
+    }
+    next(e);
+  }
 });
 // ----------------------------------------
 
--- server/src/routes/spotify.route.ts
+++ server/src/routes/spotify.route.ts
@@ -4,11 +4,12 @@ import SpotifyService from '../services/spotify.service.js';
 import createError from 'http-errors';
 
 const debug = Debug('sf-api:route:spotfiy');
+const spotfiy = new SpotifyService();
 const router = express.Router();
+const rootRoute = '/spotify';
 
-router.get('/artist-sample/:artistid/:country', async (req, res, next) => {
+router.get(`${rootRoute}/artist-sample/:artistid/:country`, async (req, res, next) => {
   try {
-    const spotfiy = new SpotifyService();
     const response = await spotfiy.getArtistSample(req.params.artistid, req.params.country);
     res.send(response);
   } catch(e) {
@@ -16,12 +17,9 @@ router.get('/artist-sample/:artistid/:country', async (req, res, next) => {
   }
 });
 
-router.get('/search/artist', async (req, res, next) => {
+router.get(`${rootRoute}/search/artist`, async (req, res, next) => {
   try {
     const searchTerm: string = (req.query.q as string);
-    debug(searchTerm);
-
-    const spotfiy = new SpotifyService();
     const response = await spotfiy.findArtist(searchTerm);
     res.send(response);
   } catch (e) {
