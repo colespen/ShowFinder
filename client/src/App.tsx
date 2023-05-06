74230915cf562f01766b4025099532ea849cd2e4
Spencer Cole <spencerlewiscole@gmail.com>
2023-05-06 18:08:01 -0400

    fix Map entry in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import Map from './components/Map';
 import './styles.scss';
-
-
 
 function App() {
 
   return (
     <div className="App">
+      <Map/>
     </div>
   );
 }
