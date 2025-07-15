f6daa92fca1b97e04ff69517128cb2203243268c
Spencer Cole <spencerlewiscole@gmail.com>
2025-07-15 11:14:32 -0400

    remove deprecated directories _client and _server

--- _server/src/utils/currAddressFilter.js
+++
@@ -1,7 +0,0 @@
-const filterCurrentAddress = (currentAddress) => {
-  return currentAddress.address.country
-    ? currentAddress.address.city + ", " + currentAddress.address.country
-    : currentAddress.address.city;
-};
-
-module.exports = filterCurrentAddress;
