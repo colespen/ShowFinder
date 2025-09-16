58db467e0984ae3617b616513478d002a5b2bdff
Spencer Cole <spencerlewiscole@gmail.com>
2025-09-16 16:49:46 -0400

    safari ios on small device layout adjust

--- client/src/hooks/useChromeIOSAdjustment.ts
+++ client/src/hooks/useChromeIOSAdjustment.ts
@@ -8,14 +8,22 @@ export const useChromeIOSAdjustment = (): void => {
   useEffect(() => {
     const userAgent = navigator.userAgent;
     const isChromeIOS = /CriOS/.test(userAgent);
+    const isSafariIOS = /iPad|iPhone|iPod/.test(userAgent) && /Safari/.test(userAgent) && !isChromeIOS;
     const isIPhone12Mini =
       window.innerWidth <= 375 && window.innerHeight <= 812;
 
-    // apply 10px adjustment if it's iPhone 12 Mini and Chrome iOS
+    // apply 15px adjustment if it's iPhone 12 Mini and Chrome iOS
     if (isIPhone12Mini && isChromeIOS) {
       document.documentElement.style.setProperty(
         "--chrome-ios-adjustment",
-        "10px"
+        "15px"
+      );
+    }
+    // apply negative adjustment for Safari on small devices to reduce gap
+    else if (isIPhone12Mini && isSafariIOS) {
+      document.documentElement.style.setProperty(
+        "--chrome-ios-adjustment",
+        "-10px"
       );
     } else {
       document.documentElement.style.setProperty(
